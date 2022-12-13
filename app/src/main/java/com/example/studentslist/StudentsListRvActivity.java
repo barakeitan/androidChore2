3ba190350ef717ca133844668822aef8b1d7794f
Barak Eitan <barakeitan48@gmail.com>
2022-12-13 18:03:50 +0200

    error in AndStd prog so initial commit again

---
+++ app/src/main/java/com/example/studentslist/StudentsListRvActivity.java
@@ -0,0 +1,14 @@
+package com.example.studentslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentsListRvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_list_rv);
+    }
+}
